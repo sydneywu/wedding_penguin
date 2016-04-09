bdff16c8b0675bd921f7d81869e62406182c2cc6
sydneywu <sydneywhz@gmail.com>
2016-04-10 02:28:47 +0800

    add input for guest list

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -2,9 +2,22 @@ var app = angular.module('WeddingPenguin',[]);
 
 app.controller('GuestCtrl',['$scope', function($scope){
 	$scope.guests = [
-	{'name':'mary', 'table': 1, 'relation': 'family'}, 
-	{'name':'mary', 'table': 1, 'relation': 'family'},
-	{'name':'mary', 'table': 1, 'relation': 'family'} 
+		{name:'mary', table: 1, relation: 'family'}, 
+		{name:'mary', table: 1, relation: 'family'}, 
+		{name:'mary', table: 1, relation: 'family'}, 
 	
 	]
+
+	$scope.addGuest = function(){
+		$scope.guests.push({name: $scope.guestName, table: $scope.guestTable, relation: $scope.guestRelation});
+		$scope.name="Guest's Name";
+		$scope.relation="Relation"
+	}
+
+	$scope.totalTables = 5
+
+	$scope.getNumber=function(num){
+		return new Array(num);
+	}
+
 }])
